7c944bbdfa218657d78577c7c09e51fc7e51dc04
miti <2220399@ecc.ac.jp>
2023-06-15 11:26:45 +0900

    change

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/kintone', [App\Http\Controllers\KintoneController::class, 'getDataFromKintone']);
