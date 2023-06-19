620924f88ac909796b8ee138dbbaa87a9a95c058
miti <2220399@ecc.ac.jp>
2023-06-19 09:21:43 +0900

    change

--- app/Http/Controllers/GetTestController.php
+++ app/Http/Controllers/GetTestController.php
@@ -8,7 +8,8 @@ class GetTestController extends Controller
 {
     //curl.exe -X 'GET' 'https://xd3utnv17sc2.cybozu.com/k/v1/record.json?app=5&id=1' -H 'X-Cybozu-API-Token: onEZOHK2ueJA5ldNinvNpz6ShgdEv0fOSBkWZacD'
     //app=5->取得するアプリID
-    public function index() {
+    public function index()
+    {
         $url = 'https://xd3utnv17sc2.cybozu.com/k/v1/record.json?app=5&id=1';
         $headers = [
             'X-Cybozu-API-Token: onEZOHK2ueJA5ldNinvNpz6ShgdEv0fOSBkWZacD'
@@ -19,16 +20,18 @@ class GetTestController extends Controller
 
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($curl, CURLOPT_CAINFO, 'C:\Users\2220415\Desktop\myapp/cacert.pem'); // cacert.pemファイルのパスを指定
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_CAINFO, 'C:\Users\2220399\Desktop\myapp-1\cacert.pem'); // cacert.pemファイルのパスを指定
 
         $response = curl_exec($curl);
-        if($response === false) {
+        if ($response === false) {
             $error = curl_error($curl);
             $errorNo = curl_errno($curl);
 
             dd("cURLエラー: " . $error . " (" . $errorNo . ")");
         }
-        dd($response);
+        $responseData = json_decode($response, true); // レスポンスを連想配列に変換
+        dd($responseData);
 
         curl_close($curl);
     }
